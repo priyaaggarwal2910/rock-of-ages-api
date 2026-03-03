a708583a76e536ec01ddb18fc2d934c3c9ab3380
priyaaggarwal2910 <priyaaggarwal2910@gmail.com>
2026-03-03 09:29:03 -0600

    Add CI/CD workflow for testing, building, and deploying to AWS

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rockapi.views import (
     register_user, login_user,
     TypeView, RockView
@@ -10,7 +13,14 @@ router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'types', TypeView, 'type')
 router.register(r'rocks', RockView, 'rock')
 
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def api_root(request):
+    return Response({"message": "Rock API is running"})
+
 urlpatterns = [
+    path('', api_root),
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
